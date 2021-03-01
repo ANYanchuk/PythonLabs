28a3b9720b51c681dc253c72bcb08b9113f9fff3
Andrey Yanchuk <and.yanchuk@gmail.com>
2021-03-01 13:16:37 +0200

    Updated JSON read and write

--- ЛР1/functions.py
+++ ЛР1/functions.py
@@ -1,56 +1,91 @@
 import json
-import csv
-import os.path
-lst = []
+import os
+from tabulate import tabulate
 
 
-def acceptance_criteria(fn):
-    def wrapper(obj):
-        if obj['fuel_used'] > 0 and obj['cars_count'] > 0:
-            fn(obj)
+def validate_data(process_func):
+    def arguments_wrapper(self, input_line: str):
+        processed_data = input_line.split(',')
+        if len(processed_data) != 3:
+            print('Слишком мало значений!')
+        elif not processed_data[0] or processed_data[0].strip().isdigit():
+            print('Фамилия некорректна!')
+        elif not processed_data[1].strip().isdigit() or not processed_data[2].strip().isdigit():
+            print('Значение удержаных или выданных денег некорректно!')
         else:
-            print('ban')
-    return wrapper
+            return process_func(self, input_line)
+
+    return arguments_wrapper
 
 
-def print_data(data):
-    print('D\t\tFU\t\tC\t\tAVG')
-    for i in data:
-        print(
-            f"{i['depot']}\t\t{i['fuel_used']}\t\t{i['cars_count']}\t\t{i['avg_fuel_used']}")
+class JsonManager(object):
+    def process_file(self):
+        processes = {'r': self.process_reading, 'w': self.process_writing}
+        process = input('\033[38mВыберите режим : чтение(r) / запись(w)\n')
+        if process and process in processes:
+            file = input('\033[38mВведите путь к файлу\n')
+            processes[process.lower()](file)
+
+    def process_writing(self, file):
+        if file.endswith('.json'):
+            if not os.path.exists(file):
+                open(file, 'a').close()
+            with open(file) as read_file:
+                file_data = json.load(read_file) if os.stat(file).st_size else []
+            with open(file, 'w') as output_file:
+                print('Вводите данные через запятую в порядке:')
+                print("Автобаза, Витрачено палива, кг., Кiлькiсть автомашин")
+                result_data = []
+                table_data = []
+                while input_string := input():
+                    parsed_data = self.parse_input(input_string)
+                    if parsed_data:
+                        result_data.append(parsed_data)
+                        table_data.append(input_string.split(',') + [parsed_data['avg']])
+                write_table = tabulate(table_data,
+                                       headers=['Автобаза', 'Витрачено палива, кг.', 'Кiлькiсть автомашин', 'Витрачено у середньому'])
+                while write := input(f'{write_table}\nЗаписать выбранные данные?(Y/n)\n'):
+                    if write.lower() == 'y':
+                        if file_data:
+                            output_file.write(json.dumps(file_data + result_data))
+                        else:
+                            output_file.write(json.dumps(result_data))
+                        print('Запись в файл окончена')
+                        break
+                    elif write.lower() == 'n':
+                        print('Запись в файл отменена')
+                        break
+        else:
+            print('\033[31mФайл должен иметь разрешение .json')
+            self.process_file()
+
+    @validate_data
+    def parse_input(self, input_line: str):
+        parsed_data = [i.strip() for i in input_line.split(',')]
+        return {'depot_name': parsed_data[0], 'fuel_used': float(parsed_data[1]), 'count': float(parsed_data[2]),
+                'avg': float(parsed_data[1]) / float(parsed_data[2])}
+
+    def process_reading(self, file):
+        if not file.endswith('.json'):
+            print('\033[31mФайл должен иметь разрешение .json')
+        elif not os.path.exists(file):
+            print('\033[31mФайл не существует')
+        else:
+            self._print_file_data(file)
+
+    @staticmethod
+    def _print_file_data(file):
+        result_data = []
+        rows = int(input('\033[38mВведите количество выводимых строк\n'))
+        with open(file, 'r') as json_file:
+            data = json.load(json_file)
+            for idx, val in enumerate(data):
+                if idx <= rows - 1:
+                    result_data.append([val['depot_name'], val['fuel_used'], val['count'], val['avg']])
+                else:
+                    break
+            print(tabulate(result_data, headers=['Автобаза', 'Витрачено палива, кг.', 'Кiлькiсть автомашин', 'Витрачено у середньому']))
 
 
-@acceptance_criteria
-def add(obj):
-    lst.append(obj)
-
-
-def input_depot():
-    for i in range(int(input('Сколько автобаз нужно добавить?'))):
-        obj = {}
-        obj['depot'] = input('Название автобазы: ')
-        obj['fuel_used'] = used = float(input('Топлива потрачено(кг): '))
-        obj['cars_count'] = cars = int(input('Количество машин: '))
-        obj['avg_fuel_used'] = used/cars
-        add(obj)
-
-
-def save_json(name):
-    input_depot()
-    with open(name, 'w') as out:
-        json.dump(lst, out)
-    lst.clear()
-
-
-def save_csv(name):
-    input_depot()
-    with open(name, 'w') as out:
-        w = csv.DictWriter(out, lst[0].keys())
-        w.writeheader()
-        for l in lst:
-            w.writerow(l)
-
-
-def load_json(name):
-    with open(name, 'r') as read:
-        print_data(json.load(read))
+def process_csv():
+    print('CSV обработан')
--- ЛР1/menu.py
+++ ЛР1/menu.py
@@ -1,19 +1,21 @@
-from functions import load_json, save_json, save_csv
+from functions import JsonManager
 
-import msvcrt
+manager = JsonManager()
 
-case = {'1': lambda: load_json(input('Введите название файла для чтения: ')),
-        '2': lambda: save_json(input('Введите название файла для записи: ')),
-        '3': lambda: save_csv(input('Введите название файла для записи: ')),
-        '4': lambda: exit(),
-        }
+
+def process_csv():
+    print('Обработка CSV не реализована')
+
+
+cases = {'json': manager.process_file, 'csv': process_csv}
 
 
 def menu():
-    print('1. Прочитать данные из файла: ')
-    print('2. Записать данные в json файл: ')
-    print('3. Записать данные в csv файл: ')
-    print('4. Выйти')
-    sign = msvcrt.getch().decode('ASCII')
-    case[sign]()
-    menu()
+    file = input("\033[38mВведите тип файла!\n")
+    try:
+        cases[file.lower()]()
+    except KeyError:
+        print(f"\033[31mНедоступный тип тип файла!\nCSV / JSON")
+    write = input("Выйти? (y/n)")
+    if write.lower() != 'y':
+        menu()
